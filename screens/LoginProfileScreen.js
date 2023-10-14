d67795cd5318e2bbb8c8a4601bc33e7f4aab09bd
Crocou <128105185+Crocou@users.noreply.github.com>
2023-10-14 16:34:49 +0900

    Update LoginProfileScreen.js

--- screens/LoginProfileScreen.js
+++ screens/LoginProfileScreen.js
@@ -1,9 +1,17 @@
 import React, { useState, useEffect } from 'react';
-import { 
-  SafeAreaView, StyleSheet, TextInput, Text, View, 
-  TouchableWithoutFeedback, Keyboard, TouchableOpacity 
+import {
+  SafeAreaView, StyleSheet, TextInput, Text, View,
+  TouchableWithoutFeedback, Keyboard, TouchableOpacity
 } from 'react-native';
 import { ToggleButton } from 'react-native-paper';
+
+//임의의 토큰 값 설정, 사용 
+//다음 화면에서도 사용
+const generateRandomToken = () => {
+  return Math.random().toString(36).substring(2) + Date.now().toString(36);
+}
+const token = generateRandomToken(); 
+//
 
 const LoginProfileScreen = ({ navigation }) => {
   const [height, setHeight] = useState('');
@@ -11,10 +19,23 @@ const LoginProfileScreen = ({ navigation }) => {
   const [birthYear, setBirthYear] = useState('');
   const [selectedGender, setSelectedGender] = React.useState(null);
   const [activityLevel, setActivityLevel] = useState(null);
+  const [showYearWarning, setShowYearWarning] = useState(false);
 
   const onGenderToggle = (gender) => {
     setSelectedGender(gender);
   };
+
+  const handleBirthYearChange = (value) => {
+    const numericValue = parseInt(value, 10);
+    const currentYear = new Date().getFullYear();
+
+    if (numericValue < 1920 || numericValue > currentYear) {
+      setShowYearWarning(true);
+    } else {
+      setShowYearWarning(false);
+    }
+    setBirthYear(value);
+};
 
   const calculateSugarIntake = () => {
     const heightInMeters = parseFloat(height) / 100;
@@ -22,10 +43,10 @@ const LoginProfileScreen = ({ navigation }) => {
 
     let activityMultiplier;
     switch (activityLevel) {
-        case 'low': activityMultiplier = 27.5; break;
-        case 'medium': activityMultiplier = 32.5; break;
-        case 'high': activityMultiplier = 37.5; break;
-        default: activityMultiplier = 0;
+      case 'low': activityMultiplier = 27.5; break;
+      case 'medium': activityMultiplier = 32.5; break;
+      case 'high': activityMultiplier = 37.5; break;
+      default: activityMultiplier = 0;
     }
 
     const dailyIntake = standardWeight * activityMultiplier;
@@ -46,8 +67,26 @@ const LoginProfileScreen = ({ navigation }) => {
     console.log("몸무게:", weight);
     console.log("태어난 연도:", birthYear);
     console.log("성별:", selectedGender);
-    navigation.navigate('LoginProfile2', { sugarGrams: sugar.grams });
+  
+    const currentYear = new Date().getFullYear();
+    const age = currentYear - parseInt(birthYear);
+    const token = generateRandomToken();
+  
+    const userData = {
+      u_code: null,
+      u_token: token,
+      u_name: null,
+      height: parseInt(height),
+      weight: parseInt(weight),
+      age: age,
+      sex: selectedGender,
+      activity_level: activityLevel,
+      u_sugar_gram: sugar.grams
+    };
+  
+    navigation.navigate('LoginProfile2', { sugarGrams: sugar.grams, userData: userData });
   };
+  
 
   const [isButtonActive, setIsButtonActive] = useState(false);
 
@@ -67,12 +106,12 @@ const LoginProfileScreen = ({ navigation }) => {
           <View style={styles.inputContainer}>
             {renderInputComponent("키", height, setHeight, "numeric", "cm")}
             {renderInputComponent("몸무게", weight, setWeight, "numeric", "kg")}
-            {renderInputComponent("태어난 연도", birthYear, setBirthYear, "numeric", "년")} 
+            {renderInputComponent("태어난 연도", birthYear, setBirthYear, "numeric", "년", handleBirthYearChange, showYearWarning)}
             {renderLabel("성별")}
             <View style={styles.genderContainer}>
               <ToggleButton
-                icon={({ color }) => 
-                  <Text style={{color: selectedGender === 'male' ? 'white' : 'lightgray', fontSize: 12}}>
+                icon={({ color }) =>
+                  <Text style={{ color: selectedGender === 'male' ? 'white' : 'lightgray', fontSize: 12 }}>
                     남성
                   </Text>}
                 value="male"
@@ -88,8 +127,8 @@ const LoginProfileScreen = ({ navigation }) => {
                 ]}
               />
               <ToggleButton
-                icon={({ color }) => 
-                  <Text style={{color: selectedGender === 'female' ? 'white' : 'lightgray', fontSize: 12}}>
+                icon={({ color }) =>
+                  <Text style={{ color: selectedGender === 'female' ? 'white' : 'lightgray', fontSize: 12 }}>
                     여성
                   </Text>}
                 value="female"
@@ -107,86 +146,87 @@ const LoginProfileScreen = ({ navigation }) => {
             </View>
             {renderLabel("활동지수")}
             <View style={styles.activityContainer}>
-                <ToggleButton
-                    icon={({ color }) => 
-                        <Text style={{color: activityLevel === 'low' ? 'white' : 'lightgray', fontSize: 12}}>
-                            적음
-                        </Text>}
-                    value="low"
-                    status={activityLevel === 'low' ? 'checked' : 'unchecked'}
-                    onPress={() => setActivityLevel('low')}
-                    style={[
-                        styles.activityButton,
-                        {
-                            backgroundColor: activityLevel === 'low' ? '#9747FF' : 'white',
-                            borderColor: 'lightgray',
-                            borderWidth: activityLevel === 'low' ? 0 : 0.8
-                        }
-                    ]}
-                />
-                <ToggleButton
-                    icon={({ color }) => 
-                        <Text style={{color: activityLevel === 'medium' ? 'white' : 'lightgray', fontSize: 12}}>
-                            보통
-                        </Text>}
-                    value="medium"
-                    status={activityLevel === 'medium' ? 'checked' : 'unchecked'}
-                    onPress={() => setActivityLevel('medium')}
-                    style={[
-                        styles.activityButton,
-                        {
-                            backgroundColor: activityLevel === 'medium' ? '#9747FF' : 'white',
-                            borderColor: 'lightgray',
-                            borderWidth: activityLevel === 'medium' ? 0 : 0.8
-                        }
-                    ]}
-                />
-                <ToggleButton
-                    icon={({ color }) => 
-                        <Text style={{color: activityLevel === 'high' ? 'white' : 'lightgray', fontSize: 12}}>
-                            많음
-                        </Text>}
-                    value="high"
-                    status={activityLevel === 'high' ? 'checked' : 'unchecked'}
-                    onPress={() => setActivityLevel('high')}
-                    style={[
-                        styles.activityButton,
-                        {
-                            backgroundColor: activityLevel === 'high' ? '#9747FF' : 'white',
-                            borderColor: 'lightgray',
-                            borderWidth: activityLevel === 'high' ? 0 : 0.8
-                        }
-                    ]}
-                />
+              <ToggleButton
+                icon={({ color }) =>
+                  <Text style={{ color: activityLevel === 'low' ? 'white' : 'lightgray', fontSize: 12 }}>
+                    적음
+                  </Text>}
+                value="low"
+                status={activityLevel === 'low' ? 'checked' : 'unchecked'}
+                onPress={() => setActivityLevel('low')}
+                style={[
+                  styles.activityButton,
+                  {
+                    backgroundColor: activityLevel === 'low' ? '#9747FF' : 'white',
+                    borderColor: 'lightgray',
+                    borderWidth: activityLevel === 'low' ? 0 : 0.8
+                  }
+                ]}
+              />
+              <ToggleButton
+                icon={({ color }) =>
+                  <Text style={{ color: activityLevel === 'medium' ? 'white' : 'lightgray', fontSize: 12 }}>
+                    보통
+                  </Text>}
+                value="medium"
+                status={activityLevel === 'medium' ? 'checked' : 'unchecked'}
+                onPress={() => setActivityLevel('medium')}
+                style={[
+                  styles.activityButton,
+                  {
+                    backgroundColor: activityLevel === 'medium' ? '#9747FF' : 'white',
+                    borderColor: 'lightgray',
+                    borderWidth: activityLevel === 'medium' ? 0 : 0.8
+                  }
+                ]}
+              />
+              <ToggleButton
+                icon={({ color }) =>
+                  <Text style={{ color: activityLevel === 'high' ? 'white' : 'lightgray', fontSize: 12 }}>
+                    많음
+                  </Text>}
+                value="high"
+                status={activityLevel === 'high' ? 'checked' : 'unchecked'}
+                onPress={() => setActivityLevel('high')}
+                style={[
+                  styles.activityButton,
+                  {
+                    backgroundColor: activityLevel === 'high' ? '#9747FF' : 'white',
+                    borderColor: 'lightgray',
+                    borderWidth: activityLevel === 'high' ? 0 : 0.8
+                  }
+                ]}
+              />
             </View>
           </View>
         </View>
-        <TouchableOpacity 
+        <TouchableOpacity
           style={[
             styles.submitButton,
             isButtonActive ? {} : { backgroundColor: 'lightgray' }
-          ]} 
+          ]}
           onPress={isButtonActive ? handleSetProfile : null}
-          >
-          <Text style={styles.submitButtonText}>설정하기</Text> 
+        >
+          <Text style={styles.submitButtonText}>설정하기</Text>
         </TouchableOpacity>
       </SafeAreaView>
     </TouchableWithoutFeedback>
   );
 };
 
-const renderInputComponent = (label, value, setValue, keyboardType, unit) => (
+const renderInputComponent = (label, value, setValue, keyboardType, unit, yearHandler, showYearWarning) => (
   <View style={styles.inputWrapper}>
     <Text>{label}</Text>
     <View style={styles.inputWithUnit}>
       <TextInput
         style={styles.input}
-        onChangeText={setValue}
+        onChangeText={yearHandler || setValue}
         value={value}
         keyboardType={keyboardType}
       />
       {unit && <Text style={styles.unitText}>{unit}</Text>}
     </View>
+    {label === "태어난 연도" && showYearWarning ? <Text style={styles.warningText}>잘못된 정보입니다</Text> : null}
   </View>
 );
 
@@ -198,10 +238,10 @@ const renderLabel = (label) => (
 
 const styles = StyleSheet.create({
   container: {
-    backgroundColor:'white',
+    backgroundColor: 'white',
     flex: 1,
     alignItems: 'center',
-    justifyContent: 'flex-start',  
+    justifyContent: 'flex-start',
   },
   header: {
     fontSize: 24,
@@ -209,7 +249,7 @@ const styles = StyleSheet.create({
     marginTop: 20
   },
   inputContainer: {
-    justifyContent: 'center',  
+    justifyContent: 'center',
     width: '100%',
     alignItems: 'center',
   },
@@ -254,7 +294,7 @@ const styles = StyleSheet.create({
     justifyContent: 'center',
     alignItems: 'center',
     borderRadius: 30,
-    marginTop: 30, 
+    marginTop: 30,
   },
   submitButtonText: {
     color: 'white',
@@ -271,6 +311,11 @@ const styles = StyleSheet.create({
   profileContainer: {
     width: '100%',
     alignItems: 'center',
+  },
+  warningText: {
+    color: 'red',
+    marginTop: 5,
+    fontSize: 12
   },
 });
 
